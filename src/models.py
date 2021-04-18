ebbfc63cf5c32e5e1693798892a75639b9b39727
rroussmolina <rroussmolina@gmail.com>
2021-04-18 02:52:42 +0000

    model instagram

--- src/models.py
+++ src/models.py
@@ -8,23 +8,37 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), nullable=False)
+    firstname = Column(String(150), nullable=False)
+    lastname = Column(String(150), nullable=False)
+    email = Column(String(250), nullable=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
+    user = relationship(User)
+
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(550), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    post = relationship(Post)
+
+
 
     def to_dict(self):
         return {}
